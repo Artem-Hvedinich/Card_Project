574efe924694a8c8421526c249a8a3184699a289
Artem-Hvedinich <artem33771@gmail.com>
2022-05-10 09:12:47 +0300

    Pagination & Arrow styled

--- src/components/ProfileGeneral/PacksList/AllPacks/Pagination.tsx
+++ src/components/ProfileGeneral/PacksList/AllPacks/Pagination.tsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import styled from "styled-components";
 import {Button} from '../../../StylesComponents/Button';
 import {ButtonProfile} from "../../../StylesComponents/ProfileAndPacksWrapper";
+import {colors} from "../../../StylesComponents/Colors";
+import {Arrow} from "../../../../UtilsFunction/Arrow";
 
 
 export const Pagination = ({totalItemsCount, pageSize, onPageChanged, portionSize, currentPage}: {
@@ -19,20 +21,20 @@ export const Pagination = ({totalItemsCount, pageSize, onPageChanged, portionSiz
     const [portionNumber, setPortionNumber] = useState(1)
     const leftPositionPageNumber = (portionNumber - 1) * portionSize + 1
     const rightPositionPageNumber = portionNumber * portionSize
+
     return (
         <NumbersWrapper>
-            {portionNumber > 1 && <ButtonProfile color={'#ffffff'} bgColor={'#110e0e'} width={4} height={1.5}
-                                          onClick={() => setPortionNumber(portionNumber - 1)}>Prev</ButtonProfile>}
+            {portionNumber > 1 && <Arrow rotate={'135'} onClick={() => setPortionNumber(portionNumber - 1)}/>}
 
             {pages.filter(p => p >= leftPositionPageNumber && p <= rightPositionPageNumber)
                 .map((p) => <PageWrap onClick={() => onPageChanged(p)}
                                       active={currentPage === p}>{p}</PageWrap>)}
             {portionCount > portionNumber &&
-                <ButtonProfile color={'#ffffff'} bgColor={'#110e0e'} width={4} height={1.5}
-                        onClick={() => setPortionNumber(portionNumber + 1)}>Next</ButtonProfile>}
+                <Arrow rotate={'-45'} onClick={() => setPortionNumber(portionNumber + 1)}/>}
         </NumbersWrapper>
     )
 }
+
 const NumbersWrapper = styled.div`
   display: flex;
   justify-content: space-around;
@@ -46,13 +48,15 @@ const PageWrap = styled.span<{ active: boolean }>`
   justify-content: center;
   width: 2vw;
   height: 2vw;
-  background: ${({active}) => active && '#4d655b'};
-  border-radius: ${({active}) => active && 10}%;
+  background: ${({active}) => active && colors.Blue};
+  color: ${({active}) => active && colors.Lavender};
+  border-radius: 10%;
   cursor: pointer;
-  font-size: ${({active}) => active ? 18 : 14}px;
-
+  font-size: ${({active}) => active ? 0.9 : 0.8}vw;
+  transition: all 0.3s;
   :hover {
-    background: grey;
-    border-radius: 50%;
-    font-size: 18px;
+    background: ${colors.LightPink};
+    border-radius: 10%;
+    color: ${colors.TextColor};
+    font-size: 0.9vw;
   }`
